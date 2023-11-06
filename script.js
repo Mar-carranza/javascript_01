af422c77ecdc5fb8fce5d4f845eaba835b32247f
mar <maria.carranzavaldes@gmail.com>
2023-11-06 13:20:54 +0100

    Practicas de javascript básico

---
+++ script.js
@@ -0,0 +1,60 @@
+// variables 
+let numero1 = 15
+console.log(typeof numero1);
+
+let numero2 = 15.5 //variable tipo decimal
+console.log(typeof numero2);
+
+let texto0 = "Esto es un texto"
+console.log(typeof texto0);
+
+let verdadero = true
+console.log(typeof verdadero);
+
+//document.getElementById("js1").innerText = numero1
+console.log(numero1);
+
+// variables numericas
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+let num4 = 4;
+
+num1 = 10;
+
+let suma = num1 + num2 + num3 + num4;
+console.log(suma);
+
+let resta = num1 - num2 - num3 - num4;
+console.log(resta);
+
+let multiplicacion = num1 * num2 * num3 * num4;
+console.log(multiplicacion);
+
+let division = num1 / num2;
+console.log(division);
+
+let potencia = num1 ** num2;
+console.log(potencia);
+
+let modulo = num1 % num2;
+console.log(modulo);
+
+console.log("la suma es", num1 + num2 + num3 + num4);
+
+
+// STRING 
+let texto1 = "Buenos"
+let texto2 = "Dias"
+
+textoTotal = texto1 + " " + texto2
+console.log(textoTotal);
+
+
+console.log("El texto tiene", textoTotal.length, "caracteres");
+console.log(textoTotal.toUpperCase());
+
+let num5 = 5;
+//num5 = num5 + 4;
+num5 += 4
+console.log("num5 vale", num5);
